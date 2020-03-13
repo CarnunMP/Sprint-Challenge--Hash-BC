4fbe3679c519edd776b99473ae58dfdc49810016
CarnunMP <carnun@hotmail.co.uk>
2020-03-13 11:39:12 +0000

    11:37 — Ex1 tests passing!

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -13,13 +13,25 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     for i, weight in enumerate(weights):
-        hash_table_insert(ht, weight, i)
+        existing_ht_entry = hash_table_retrieve(ht, weight)
+        if existing_ht_entry == None:
+            hash_table_insert(ht, weight, [i])
+        else:
+            hash_table_insert(ht, weight, existing_ht_entry + [i])
 
     for weight in weights:
         limit_minus_weight = hash_table_retrieve(ht, limit - weight)
 
         if limit_minus_weight != None:
-            weight_index = hash_table_retrieve(ht, weight)
+            weight_index = hash_table_retrieve(ht, weight)[0]
+
+            if limit - weight == weight:
+                # print(limit_minus_weight)
+                weight_index = limit_minus_weight[1]
+            
+            limit_minus_weight = limit_minus_weight[0]
+
+            # print((max(limit_minus_weight, weight_index), min(limit_minus_weight, weight_index)))
 
             return (max(limit_minus_weight, weight_index), min(limit_minus_weight, weight_index))
 
